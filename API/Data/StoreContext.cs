139136dfbcf14ceb9a966b7cd77e60509f7f662c
Ntd93 <nguyentiendung2003@gmail.com>
2023-09-09 23:30:19 +0700

    Add project files.

---
+++ API/Data/StoreContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+public class StoreContext : DbContext
+{
+	public StoreContext(DbContextOptions options) : base(options)
+	{
+
+    }
+
+	public DbSet<Product> Products { get; set; }
+}

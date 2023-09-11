4af618829ade1bac054b62596b5ba420a82b1cc5
Ntd93 <nguyentiendung2003@gmail.com>
2023-09-11 17:14:31 +0700

    done page list products

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,19 +1,22 @@
 import { Product } from "../../app/models/product";
+import ProductList from "./ProductList";
+import { useState, useEffect } from "react";
 
-interface Props {
-    products: Product[];
-    addProduct: () => void;
-}
+export default function Catalog() {
+    const [products, setProducts] = useState<Product[]>([])
 
-export default function Catalog({ products, addProduct }: Props) {
+    useEffect(() => {
+        fetch('http://localhost:5000/api/products')
+            .then(response => response.json())
+            .then(data => setProducts(data))
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }, [])
+
     return (
         <>
-            <ul>
-                {products.map((product) => (
-                    <li key={product.id}>{product.name} - {product.price}</li>
-                ))}
-            </ul>
-            <button onClick={addProduct}>Add Product</button>
+            <ProductList products={products} ></ProductList>
         </>
     )
 }

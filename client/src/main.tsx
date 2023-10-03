48530b7ed1e2fb4cafe2f036d1c014d4a82a3204
Ntd93 <nguyentiendung2003@gmail.com>
2023-10-03 09:44:45 +0700

    apply redux in basket management

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app/layout/App.tsx'
 import './app/layout/style.css'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -8,13 +7,13 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/router/Router.tsx';
-import { StoreProvider } from './app/context/StoreContext.tsx';
-
+import { Provider } from 'react-redux';
+import { store } from './app/store/configureStore.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <StoreProvider>
+    <Provider store={store}>
       <RouterProvider router={router} />
-    </StoreProvider>
+    </Provider>
   </React.StrictMode>,
 )
